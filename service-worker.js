85fe136869b65312740b73d9f59e4081ef3fb764
lefeuvth <118385221+lefeuvth@users.noreply.github.com>
2025-07-03 13:54:14 +0200

    Add buttons to notifications

--- service-worker.js
+++ service-worker.js
@@ -238,7 +238,10 @@ self.addEventListener('push', function(event) {
     action_primary: data.action_primary,
     action_secondary: data.action_secondary,
     ...(data.sender && { sender: data.sender }),
-    ...(data.receiver && { receiver: data.receiver })    
+    ...(data.receiver && { receiver: data.receiver }),
+    ...(data.creatorAccount && { creatorAccount: data.creatorAccount }),
+    ...(data.coinbase && { coinbase: data.coinbase })    
+    ...(data.coinbaseReceiverAccount && { coinbaseReceiverAccount: data.coinbaseReceiverAccount })      
   };
 
   if (!title || !message_id) {
