39bafb9567943086b627f73e4e7cf38f6dad3c21
lefeuvth <118385221+lefeuvth@users.noreply.github.com>
2026-08-31 10:50:24 +0200

    Add filtered native movement totals and USD values

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mina-graph-explorer-v47';
+const CACHE_NAME = 'mina-graph-explorer-v48';
 const APP_BUILD_DATE = '2026-08-30';
 const FORCE_ACTIVATE_FROM_CACHE = 'mina-graph-explorer-v37';
 
