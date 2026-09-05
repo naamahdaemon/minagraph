e2afa7e26c46db932f14d8cae61c3ffcc0134d24
lefeuvth <118385221+lefeuvth@users.noreply.github.com>
2026-09-05 22:21:29 +0200

    Refine mobile fullscreen control gestures

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mina-graph-explorer-v82';
+const CACHE_NAME = 'mina-graph-explorer-v83';
 const APP_BUILD_DATE = '2026-09-01';
 const FORCE_ACTIVATE_FROM_CACHE = 'mina-graph-explorer-v37';
 
